3d4b321aa90847b56e3028ab006bb3b3732d948f
baunai <75685937+baunai@users.noreply.github.com>
2024-07-03 14:24:55 -0500

    Add files via upload

---
+++ CloudOSD/WMIExplorer.ps1
@@ -0,0 +1,216 @@
+<#WMI Explorer
+Gary Blok @gwblok Recast Software
+
+Used with OSDCloud Edition OSD
+
+Downloads the WMIExplorer Suite directly from GitHub
+
+Creates shortcut in Start Menu for the items in $Shortcuts Variable
+Shortcut Variable based on $_.VersionInfo.InternalName of the exe file for the one you want a shortcut of.
+
+Adapted and modified by Hoang Nguyen to fit with current environment
+#>
+
+try {$tsenv = new-object -comobject Microsoft.SMS.TSEnvironment}
+catch{Write-Output "Not in TS"}
+
+$ScriptName = "WMIExplorer"
+
+$ScriptVersion = "22.03.07.01"
+if ($tsenv){
+    $LogFolder = $tsenv.value('CompanyFolder')#Company Folder is set during the TS Var at start of TS.
+    $CompanyName = $tsenv.value('CompanyName')
+    }
+if (!($CompanyName)){$CompanyName = "RecastSoftwareIT"}#If CompanyName / CompanyFolder info not found in TS Var, use this.
+if (!($LogFolder)){$LogFolder = "$env:ProgramData\$CompanyName"}
+$LogFilePath = "$LogFolder\Logs"
+$LogFile = "$LogFilePath\WMIExplorer.log"
+
+#Download & Extract to Program Files
+$FileName = "WmiExplorer_2.0.0.2.zip"
+$InstallPath = "$env:windir\System32"
+
+if ($PSVersionTable.PSVersion.Major -ne 7) {
+    Install-PackageProvider -Name NuGet -Force
+    Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+    Install-Script PS7Bootstrap -Force -ErrorAction Ignore
+    PS7Bootstrap.ps1 -$PSCommandPath
+    #Exit $LASTEXITCODE
+}
+
+function Get-TaskSequenceStatus {
+    # Determine if a task sequence is currently running
+    try {
+        $TSEnv = New-Object -ComObject Microsoft.SMS.TSEnvironment
+    }
+    catch {}
+    if ($null -eq $TSEnv) {
+        return $false
+    }
+    else {
+        try {
+            $SMSTSType = $TSEnv.Value("_SMSTSType")
+        }
+        catch {}
+        if ($null -eq $SMSTSType) {
+            return $false
+        }
+        else {
+            return $true
+        }
+    }
+}
+
+function Write-Log {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true, HelpMessage = "Message added to the log file.")]
+        [ValidateNotNullOrEmpty()]
+        [string]$Message,
+
+        [Parameter(Mandatory = $false, HelpMessage = "Set running script component")]
+        [ValidateNotNullOrEmpty()]
+        [string]$Component = "Script",
+
+        [Parameter(Mandatory = $false, HelpMessage = "Severity for the log entry. 1 for Informational, 2 for Warning, 3 for Error.")]
+        [ValidateNotNullOrEmpty()]
+        [ValidateRange(1, 3)]
+        [int16]$Severity = 1,
+
+        [Parameter(Mandatory = $false, HelpMessage = "Output script run to console host")]
+        [ValidateNotNullOrEmpty()]
+        [Boolean]$WriteHost = $true,
+
+        [Parameter(Mandatory = $false, HelpMessage = "Name of the log file that the entry will written to.")]
+        [ValidateNotNullOrEmpty()]
+        [string]$FileName = "WMIExplorer.log"
+    )
+    
+    if (Get-TaskSequenceStatus) {
+        $TSEnv = New-Object -ComObject Microsoft.SMS.TSEnvironment
+        $LogDir = $TSEnv.Value("_SMSTSLogPath")
+        $LogFilePath = Join-Path -Path $LogDir -ChildPath $FileName
+    }
+    else {
+        $LogDir = Join-Path -Path "${env:SystemRoot}" -ChildPath "Temp"
+        $LogFilePath = Join-Path -Path $LogDir -ChildPath $FileName
+    }
+
+$global:ScriptLogFilePath = $LogFilePath
+$VerbosePreference = 'Continue'
+
+if ($WriteHost) {
+    foreach ($msg in $Message) {
+        # Create script block for writting log entry to the console
+        [scriptblock]$WriteLogLineToHost = {
+            Param (
+                [string]$lTextLogLine,
+                [Int16]$lSeverity
+            )
+            if (${PSVersionTable}.PSVersion.Major -eq "7") {
+                switch ($lSeverity) {
+                    3 { $Style = "$($PSStyle.Bold)$($PSStyle.Foreground.Red)"; Write-Host "$($Style)$lTextLogLine" }
+                    2 { $Style = "$($PSStyle.Bold)$($PSStyle.Foreground.Yellow)"; Write-Host "$($Style)$lTextLogLine" }
+                    1 { $Style = "$($PSStyle.Bold)$($PSStyle.Foreground.White)"; Write-Host "$($Style)$lTextLogLine" }
+                }
+            }
+
+            if (${PSVersionTable}.PSVersion.Major -eq "5") {
+                if ($Host.UI.RawUI.ForegroundColor) {
+                    switch ($lSeverity) {
+                        3 {
+                            Write-Host -Object $lTextLogLine -ForegroundColor Red
+                        }
+                        2 {
+                            Write-Host -Object $lTextLogLine -ForegroundColor Yellow
+                        }
+                        1 {
+                            Write-Host -Object $lTextLogLine
+                        }
+                    }
+                }
+                # If executing "powershell.exe" -File <filename>.ps1 > log.txt", then all the Write-Host calls are converted to Write-Output calls so that they are included in the text log.
+                else {
+                    Write-Output -InputObject ($lTextLogLine)
+                }
+            }            
+        }
+        & $WriteLogLineToHost -lTextLogLine $msg -lSeverity $Severity 
+    }
+}
+
+    $TimeGenerated = "$(Get-Date -Format HH:mm:ss).$((Get-Date).Millisecond)+000"
+    $Line = '<![LOG[{0}]LOG]!><time="{1}" date="{2}" component="{3}" context="" type="{4}" thread="" file="">'
+    $LineFormat = $Message, $TimeGenerated, (Get-Date -Format MM-dd-yyyy), $Component, $Severity
+    $Line = $Line -f $LineFormat
+    
+    try {
+        Out-File -InputObject $Line -Append -NoClobber -Encoding Default -FilePath $LogFilePath
+    }
+    catch [System.Exception] {
+        # Exception is stored in the automatic variable _
+        Write-Warning -Message "Unable to append log entry to $($LogFilePath) file. Error message: $($_.Exception.Message)"
+    }
+
+}
+
+# Leave blank space at top of window to not block output by progress bars
+Function AddHeaderSpace {
+    Write-Output "This space intentionally left blank..."
+    Write-Output ""
+    Write-Output ""
+    Write-Output ""
+    Write-Output ""
+    Write-Output ""
+}
+
+AddHeaderSpace
+
+$Script_Start_Time = (Get-Date).ToShortDateString() + ", " + (Get-Date).ToLongTimeString()
+Write-Log -Message "INFO: Script Start: $Script_Start_Time"
+
+# Enable TLS 1.2 support for downloading modules from PSGallery (Required)
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+# Bypass Proxy
+(New-Object System.Net.WebClient).Proxy.Credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials
+#[System.Net.Http.HttpClient]::DefaultProxy = New-Object System.Net.WebProxy($null)
+
+#Setup LOCALAPPDATA Variable
+[System.Environment]::SetEnvironmentVariable('LOCALAPPDATA', "$env:SystemDrive\Windows\system32\config\systemprofile\AppData\Local")
+
+Write-Log -Message  "Running Script: $ScriptName | Version: $ScriptVersion"
+
+$URL = "https://github.com/vinaypamnani/wmie2/releases/download/v2.0.0.2/$FileName"
+$DownloadTempFile = "$env:TEMP\$FileName"
+
+Write-Log -Message  "Downloading $URL to $DownloadTempFile"
+$Download = Start-BitsTransfer -Source $URL -Destination $DownloadTempFile -DisplayName $FileName
+
+#Write-Output "Downloaded Version Newer than Installed Version, overwriting Installed Version"
+Write-Log -Message  "Expanding to $InstallPath"
+Expand-Archive -Path "$env:TEMP\$FileName" -DestinationPath $InstallPath -Force
+
+
+$App = get-item -Path "$InstallPath\WmiExplorer.exe"
+
+Write-Log -Message  "Create Shortcut for $($App.Name)"
+
+#Build ShortCut Information
+$AppName = $App.VersionInfo.ProductName
+$SourceExe = $App.FullName
+$DestinationPath = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\$($AppName).lnk"
+
+#Create Shortcut
+$WshShell = New-Object -comObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut($DestinationPath)
+$Shortcut.TargetPath = $SourceExe
+$Shortcut.Arguments = $ArgumentsToSourceExe
+$Shortcut.Save()
+                    
+$Script_End_Time = (Get-Date).ToShortDateString() + ", " + (Get-Date).ToLongTimeString()
+$Script_Time_Taken = New-TimeSpan -Start $Script_Start_Time -End $Script_End_Time
+
+Write-Log -Message "INFO: Script end: $Script_End_Time"
+Write-Log -Message "INFO: Execution time: $Script_Time_Taken"
+Write-Log -Message "***************************************************************************"
